bb6d5a426e84ea8e461394d74a10e497775408e0
Fernando Seguim <fernando.seguim@gmail.com>
2018-09-02 23:59:22 -0300

    add subtitle and message params

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,13 @@
 import React, { Fragment } from 'react';
 import './style.css';
 
-export default () => (
+export default (props) => (
     <Fragment>
         <header className="main-header">
             <h1 className="main-header-title">Campeonato de filmes</h1>
-            <h2 className="main-header-subtitle">Fase de Seleção</h2>
+            <h2 className="main-header-subtitle">{ props.subtitle }</h2>
             <hr></hr>
-            <p>Selecione 8 filmes que você deseja que entrem na competição e depois pressione o botão Gerar Meu Campeonato para prosseguir.</p>
+            <p>{ props.message }</p>
         </header>
     </Fragment>
 );
